1d8a8e1e3c22f4beca9ce5a1591de8e5459ea63a
davit tsetsadze <tsetsedavit3@gmail.com>
2024-08-09 17:44:27 +0200

    mapscreen add modal arrival

--- components/BikeDisplay.js
+++ components/BikeDisplay.js
@@ -3,43 +3,43 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export default function BikeDisplay() {
-    //Déclarer l'adresse du backend
-    const BACKEND_ADDRESS = "http://192.168.100.237:3000";
+  //Déclarer l'adresse du backend
+  const BACKEND_ADDRESS = "http://192.168.100.78:3000";
 
-    const [currentPosition, setCurrentPosition] = useState(null);
+  const [currentPosition, setCurrentPosition] = useState(null);
 
-    const [velibBike, setVelibBike] = useState([]);
+  const [velibBike, setVelibBike] = useState([]);
 
-    useEffect(() => {
-        //Récupérer les données des vélos
-        fetch(`${BACKEND_ADDRESS}/bikes`)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data.velibParkingAavailbale[0].lat);
-            });
-    }, []);
+  useEffect(() => {
+    //Récupérer les données des vélos
+    fetch(`${BACKEND_ADDRESS}/bikes`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.velibParkingAavailbale[0].lat);
+      });
+  }, []);
 
-    //afficher les vélos disponibles
-    // const displayBikes = () => {
-    //     fetch(`${BACKEND_ADDRESS}/bikes`)
-    //         .then((response) => response.json())
-    //         .then((data) => {
-    //             console.log(data);
-    //             return (
-    //                 <Bikes
+  //afficher les vélos disponibles
+  // const displayBikes = () => {
+  //     fetch(`${BACKEND_ADDRESS}/bikes`)
+  //         .then((response) => response.json())
+  //         .then((data) => {
+  //             console.log(data);
+  //             return (
+  //                 <Bikes
 
-    //                 data.velibParkingAavailable.lat;
-    //                 data.velibParkingAavailable.lon;
-    //             )
-    //         });
-    // };
-    // displayBikes();
+  //                 data.velibParkingAavailable.lat;
+  //                 data.velibParkingAavailable.lon;
+  //             )
+  //         });
+  // };
+  // displayBikes();
 
-    return (
-        <View>
-            <Text>BikeDisplay</Text>
-        </View>
-    );
+  return (
+    <View>
+      <Text>BikeDisplay</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
