4fcb02c447c84f0df88c31e2e4499d45248d8f54
davit tsetsadze <tsetsedavit3@gmail.com>
2024-08-12 14:57:26 +0200

    update alert + historique

--- screens/AlertScreen.js
+++ screens/AlertScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useEffect, useState } from "react";
@@ -8,7 +8,7 @@ export default function AlertScreen() {
 
   const [dataAlert, setDataAlert] = useState(null);
   useEffect(() => {
-    fetch(`http://192.168.100.119:3000/alerts`)
+    fetch(`http://192.168.100.78:3000/alerts`)
       .then((response) => response.json())
       .then((data) => {
         setDataAlert(data.dataOriginal);
@@ -24,30 +24,59 @@ export default function AlertScreen() {
             <FontAwesome name="bell" size={25} color="white"></FontAwesome>
           </View>
           <View style={styles.textAlertContainer}>
-            <Text style={styles.text}>Alert</Text>
+            <Text style={styles.textAlert}>ALERT</Text>
             <Text style={styles.text}>{el.name}</Text>
           </View>
         </View>
       );
     });
-  return <View style={styles.container}>{afficheAlert}</View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView} indicatorStyle="white">
+        {afficheAlert}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#303F4A",
+    alignItems: "center",
     justifyContent: "center",
   },
   alertContainer: {
+    alignContent: "center",
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "white",
+    marginBottom: "1%",
+    padding: 15,
+    width: "90%",
   },
   iconAlert: {
     alignItems: "center",
     justifyContent: "center",
+    width: "30%",
   },
   text: {
     color: "white",
+    fontSize: "16%",
+    marginBottom: "2%",
+  },
+  textAlert: {
+    color: "white",
+    fontSize: "16%",
+    marginBottom: "2%",
+    letterSpacing: 3,
+    fontWeight: "bold",
+    color: "yellow",
+  },
+  textAlertContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "70%",
   },
 });
