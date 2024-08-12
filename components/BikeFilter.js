2c62dc699c6ac1b2a036b171a7a2e85168b00210
Your name <Your email>
2024-08-12 16:34:50 +0200

    bike filter CSS

--- components/BikeFilter.js
+++ components/BikeFilter.js
@@ -2,93 +2,93 @@ import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function BikeFilter(props) {
-    //Mise en place de la mécanique d'inverse data flow pour la fonction handleFilterPress
-    const handleFilter = () => {
-        props.handleFilterPress(props.bikeType);
-    };
+  //Mise en place de la mécanique d'inverse data flow pour la fonction handleFilterPress
+  const handleFilter = () => {
+    props.handleFilterPress(props.bikeType);
+  };
 
-    return (
-        //Créer un bouton pour chaque type de filtre en utilisant la mécanique de props
-        <TouchableOpacity
-            style={styles[props.bikeType]}
-            onPress={() => handleFilter()}
-        >
-            <Text style={{ color: "#ffffff" }}>{props.bikeType}</Text>
-        </TouchableOpacity>
-    );
+  return (
+    //Créer un bouton pour chaque type de filtre en utilisant la mécanique de props
+    <TouchableOpacity
+      style={styles[props.bikeType]}
+      onPress={() => handleFilter()}
+    >
+      <Text style={{ color: "#ffffff" }}>{props.bikeType}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    btn: {
-        borderWidth: 1,
-        borderColor: "red",
-        width: 150,
+  btn: {
+    borderWidth: 1,
+    borderColor: "red",
+    width: 150,
+  },
+  velib: {
+    backgroundColor: "#2280F5",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "22%",
+    height: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    velib: {
-        backgroundColor: "#2280F5",
-        borderRadius: 10,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "22%",
-        height: 20,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
 
-    dott: {
-        backgroundColor: "#1AABEB",
-        borderRadius: 10,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "22%",
-        height: 20,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
+  dott: {
+    backgroundColor: "#1AABEB",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "22%",
+    height: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
 
-    tier: {
-        backgroundColor: "#172156",
-        borderRadius: 10,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "22%",
-        height: 20,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
+  tier: {
+    backgroundColor: "#172156",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "22%",
+    height: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
 
-    lime: {
-        backgroundColor: "#07D603",
-        borderRadius: 10,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "22%",
-        height: 20,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 5,
+  lime: {
+    backgroundColor: "#07D603",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "22%",
+    height: 25,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
 });
