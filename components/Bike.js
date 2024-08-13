61041e77f4f02141f49b6dbd2f1c39a10346371b
Adrien <ad.tranchand@gmail.com>
2024-08-13 13:55:18 +0200

    Mapscreen + Bike + BikeFilter + Alert modifications

--- components/Bike.js
+++ components/Bike.js
@@ -1,28 +1,49 @@
 import { Marker } from "react-native-maps";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { useState } from "react";
 
 export default function Bike(props) {
+    const [isPressed, setIsPressed] = useState(false);
     return (
         <Marker
             coordinate={props.coords}
             style={{ display: props.isVisible ? "flex" : "none" }}
-            onPress={() => props.onPress()}
+            /*appeler via la prop onPress la fonction qui permet d'afficher la BikeModal et modifier l'état isPressed*/
+            onPress={() => props.onPress() && setIsPressed(true)}
         >
-            <FontAwesome
-                name="bicycle"
-                color={
-                    props.bikeType === "velib"
-                        ? "#2280F5"
-                        : "lime"
-                        ? "#07D603"
-                        : "dott"
-                        ? "#1AABEB"
-                        : "tier"
-                        ? "#172156"
-                        : ""
-                }
-                size={25}
-            />
+            {isPressed ? (
+                <FontAwesome
+                    name="bicycle"
+                    color={
+                        props.bikeType === "velib"
+                            ? "#2280F5"
+                            : "lime"
+                            ? "#07D603"
+                            : "dott"
+                            ? "#1AABEB"
+                            : "tier"
+                            ? "#172156"
+                            : ""
+                    }
+                    size={35}
+                />
+            ) : (
+                <FontAwesome
+                    name="bicycle"
+                    color={
+                        props.bikeType === "velib"
+                            ? "#2280F5"
+                            : "lime"
+                            ? "#07D603"
+                            : "dott"
+                            ? "#1AABEB"
+                            : "tier"
+                            ? "#172156"
+                            : ""
+                    }
+                    size={25}
+                />
+            )}
         </Marker>
     );
 }
